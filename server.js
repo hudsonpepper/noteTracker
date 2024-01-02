e9504fb324f4d074dfcf3cffb6c9f3bac2b94126
hudsonpepper <75504341+hudsonpepper@users.noreply.github.com>
2024-01-02 17:51:25 -0500

    added console.log get and post requests for api/notes

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3001;
 const noteData = require('./db/notes.json');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 // GET Route for homepage
@@ -16,4 +19,12 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+app.get('/api/notes', (req, res) =>
+  console.info(`${req.method} request received to get notes`)
+);
+
+app.post('/api/notes', (req, res) =>
+  console.ingo(`${req.method} request received to add a new note`);
+  console.log("Note: ", req.body);
+);
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
